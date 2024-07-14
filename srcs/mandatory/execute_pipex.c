64b530270d3dbbe001da1a9d87c768b637d3fc3a
tranhieutrung <tranhieutrung@outlook.com>
2024-07-14 11:12:30 +0300

    correct execute_command, still leak

--- srcs/mandatory/execute_pipex.c
+++ srcs/mandatory/execute_pipex.c
@@ -6,7 +6,7 @@
 /*   By: hitran <hitran@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 21:15:00 by hitran            #+#    #+#             */
-/*   Updated: 2024/07/13 22:59:08 by hitran           ###   ########.fr       */
+/*   Updated: 2024/07/14 10:44:48 by hitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,27 +27,19 @@ static void	excecute_command(t_pipex *pipex, char *command)
 
 	splitted_command = split_command(command);
 	if (!splitted_command)
-	{
-		ft_printf_fd(2, "pipex: split command failed\n");
-		exit(1);
-	}
+		handle_error("split command failed", 1, NULL);
 	command_path = find_command_path(pipex, splitted_command);
-	ft_printf_fd(1, "cmd path = %s\n", command_path);
 	if (!command_path)
-		handle_error_127(*splitted_command, splitted_command);
+		handle_error("command not found", 127, splitted_command);
 	execve(command_path, splitted_command, pipex->envp);
-	handle_error_exec(command_path, splitted_command);
+	handle_exec_error(command_path, splitted_command);
 }
 
 static void	execute_child_process(t_pipex *pipex)
 {
 	pipex->fd[0] = open(pipex->argv[1], O_RDONLY);
 	if (pipex->fd[0] == -1)
-	{
-		ft_printf_fd(2, "pipex: %s: %s\n", pipex->argv[1], strerror(errno));
-		close(pipex->pipe[1]);
-		exit(1);
-	}
+		handle_open_error(pipex->argv[1], pipex->pipe[1]);
 	redirect_fds(pipex->fd[0], 0, pipex->pipe[1], 1);
 	excecute_command(pipex, pipex->argv[2]);
 }
@@ -56,50 +48,35 @@ static void	execute_parent_process(t_pipex *pipex)
 {
 	pipex->pid = fork();
 	if (pipex->pid == -1)
-	{
-		close(pipex->pipe[0]);
-		close(pipex->fd[1]);
-		perror("pipex: fork\n");
-		exit (1);
-	}
+		handle_fork_error(pipex->pipe[0], pipex->fd[1]);
 	else if (pipex->pid == 0)
 	{
 		pipex->fd[1] = open(pipex->argv[pipex->argc - 1],
 				O_CREAT | O_RDWR | O_TRUNC, 00700);
 		if (pipex->fd[1] == -1)
-		{
-			ft_printf_fd(2, "pipex: %s: %s\n",
-				pipex->argv[pipex->argc - 1], strerror(errno));
-			close(pipex->pipe[0]);
-			exit(1);
-		}
+			handle_open_error(pipex->argv[pipex->argc - 1], pipex->pipe[0]);
 		redirect_fds(pipex->pipe[0], 0, pipex->fd[1], 1);
 		excecute_command(pipex, pipex->argv[3]);
 	}
 	else
-		waitpid(pipex->pid, &pipex->error, 0);//&pipex->error
+		waitpid(pipex->pid, &pipex->error, 0);
 }
 
 void	execute_pipex(t_pipex *pipex)
 {
 	pipex->pid = fork();
 	if (pipex->pid == -1)
-	{
-		close(pipex->pipe[0]);
-		close(pipex->pipe[1]);
-		perror("pipex: fork\n");
-		exit (1);
-	}
+		handle_fork_error(pipex->pipe[0], pipex->pipe[1]);
 	else if (pipex->pid == 0)
 	{
-		close(pipex->pipe[0]); //dong pipe doc, khi thuc hien lenh cmd1 o infile se ghi ra pipe[1]
+		close(pipex->pipe[0]);
 		execute_child_process(pipex);
 	}
 	else
 	{
-		close(pipex->pipe[1]);// dong pipe[1] de khi thuc hien lenh cmd2 se doc lenh tu pipe[0]
+		close(pipex->pipe[1]);
 		execute_parent_process(pipex);
-		close(pipex->pipe[0]);// dong pipe[0] de ket thuc pipe
+		close(pipex->pipe[0]);
 		waitpid(pipex->pid, &pipex->error, 0);
 	}
 }
